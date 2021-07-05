64a47824d1ba879b630ed301f6284ae9c959b420
manhetan <manhetan@gmail.com>
2021-07-05 23:42:29 +0300

    HW1UPD

--- src/index.js
+++ src/index.js
@@ -3,20 +3,22 @@ import mailHero from './assets/Male-1-Walk.png';
 
 const canvas = document.getElementById('game');
 const context = canvas.getContext('2d');
+const canvasW = canvas.width;
+const canvasH = canvas.height;
 const spriteW = 48;
 const spriteH = 48;
 const shots = 3;
 let cycle = 0;
 let bottomPressed = false;
-let pY = 300;
-let pX = 300;
+let pY = canvasH/2;
+let pX = canvasW/2;
 let direction;
 let dirShots = 0;
 // context.strokeStyle = 'green';
 // context.lineWidth = 9;
 // context.strokeRect(20, 20, 200, 100);
 
-function keyHandler(e) {
+function keyUpHandler(e) {
   console.log(e.key);
   if (e.key === 'Down' || e.key === 'ArrowDown') {
     bottomPressed = true;
@@ -36,7 +38,28 @@ function keyHandler(e) {
   }
 }
 
-document.addEventListener('keydown', keyHandler);
+function keyDownHandler(e) {
+  console.log(e.key);
+  if (e.key === 'Down' || e.key === 'ArrowDown') {
+    bottomPressed = false;
+    direction = 'Down';
+  }
+  if (e.key === 'Up' || e.key === 'ArrowUp') {
+    bottomPressed = false;
+    direction = 'Up';
+  }
+  if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    bottomPressed = false;
+    direction = 'Left';
+  }
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    bottomPressed = false;
+    direction = 'Right';
+  }
+}
+
+document.addEventListener('keydown', keyUpHandler);
+document.addEventListener('keyup', keyDownHandler);
 
 const img = document.createElement('img');
 img.src = mailHero;
@@ -46,22 +69,26 @@ img.addEventListener('load', () => {
     if (bottomPressed && direction === 'Down') {
       pY += 10;
       cycle = (cycle + 1) % shots;
-      dirShots = 0;
+      dirShots = spriteH*0;
+      console.log(dirShots);
     }
     if (bottomPressed && direction === 'Up') {
       pY -= 10;
       cycle = (cycle + 1) % shots;
-      dirShots = 144;
+      dirShots = spriteH*3;
+      console.log(dirShots);
     }
     if (bottomPressed && direction === 'Left') {
       pX -= 10;
       cycle = (cycle + 1) % shots;
-      dirShots = 48;
+      dirShots = spriteH*1;
+      console.log(dirShots);
     }
     if (bottomPressed && direction === 'Right') {
       pX += 10;
       cycle = (cycle + 1) % shots;
-      dirShots = 96;
+      dirShots = spriteH*2;
+      console.log(dirShots);
     }
     context.clearRect(0, 0, 600, 600);
     context.drawImage(img, cycle * spriteW, dirShots, spriteW, spriteH, pX, pY, 48, 48);
