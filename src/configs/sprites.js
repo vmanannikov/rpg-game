688d081cbae8bd12324f37e325c2cac531fbab6c
manhetan <manhetan@gmail.com>
2021-07-25 22:05:04 +0300

    HW3

--- src/configs/sprites.js
+++ src/configs/sprites.js
@@ -5,24 +5,24 @@ export default {
     terrain: {
         grass: {
             img: terrainIMG,
-            frames: [[896, 256, 64, 64]]
+            frames: [[896, 256, 64, 64]],
         },
         water: {
             img: terrainIMG,
-            frames: [[0, 576, 64, 64]]
+            frames: [[0, 576, 64, 64]],
         },
         wall: {
             img: terrainIMG,
-            frames: [[448, 64, 64, 64]]
+            frames: [[448, 64, 64, 64]],
         },
         spawn: {
             img: terrainIMG,
-            frames: [[384, 640, 64, 64]]
+            frames: [[384, 640, 64, 64]],
         },
         npcSpawn: {
             img: terrainIMG,
-            frames: [[896, 576, 64, 64]]
-        }
+            frames: [[896, 576, 64, 64]],
+        },
     },
     characters: {
         girl1: {
